becd24a13a48c8294062eec916b1bb3794b36d53
Prof. Márcio Torres <marcio.torres@riogrande.ifrs.edu.br>
2021-01-20 09:14:25 -0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+// Todo projeto possui uma classe Principal, Main ou nome do projeto (como Template.java)
+class App {
+  // a classe principal possui o método principal main()
+  public static void main(String[] args) {
+    System.out.println("Olá mundo!");
+  }
+}
